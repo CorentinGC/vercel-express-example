a2f4fe21293d92145349504d53b760e593b8a3e4
CorentinGC <corentingc@gmail.com>
2025-02-17 05:47:53 +0100

    upd: get token + add middleware

--- api/index.js
+++ api/index.js
@@ -3,11 +3,18 @@ const app = express()
 const port = 4000
 
 
+//middleware
+app.use((req, res, next) => {
+    console.log('Path:', req.url)
+    next()
+})
+
 app.get('/', (req, res) => {
     res.status(200).json({code: 'HEllo World'})
 })
 
 app.get('/auth/:route', (req, res) => {
+    console.log('Token', req.headers.authorization)
     res.status(401).json({code: "unauthorized"})
 })
 
