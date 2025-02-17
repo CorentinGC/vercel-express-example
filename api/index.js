d882f234f96c571c5f19ef2491316dbd1454e9f2
CorentinGC <corentingc@gmail.com>
2025-02-17 05:51:08 +0100

    upd: simple auth check

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const isAuth = (level=1, req, res) => {
+    console.log('Token', req.headers.authorization)
 
+    if(level < 10) res.status(401).json({code: "unauthorized"})
+}
 //middleware
 app.use((req, res, next) => {
     console.log('Path:', req.url)
@@ -14,8 +18,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('/auth/:route', (req, res) => {
-    console.log('Token', req.headers.authorization)
-    res.status(401).json({code: "unauthorized"})
+    isAuth(12, req, res)
+    res.status(200).json({code: "auth"})
 })
 
 app.listen(port, () => {
