9e5d19f96e5d26008eed61b620489ac47ad601f8
CorentinGC <corentingc@gmail.com>
2025-02-17 05:20:24 +0100

    upd: test route subpath

--- api/index.js
+++ api/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.status(200).send({code: 'Hello World!'})
 })
+
+app.route('/auth')
+    .get('/', (req, res) => {
+        res.status(200).send({code: 'Login path'})
+    })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
